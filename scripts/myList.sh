db3e4b3e332427f0165768fecd4ede4618cf0e47
smipth <spaghetiwestern@gmail.com>
2015-09-02 15:47:05 -0400

    fixed ls to run from project main directory

--- scripts/myList.sh
+++ scripts/myList.sh
@@ -5,15 +5,18 @@
 
 #Code for finding directory of script using environmental variables found on stack overflow
 #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-touch $DIR/../temp/directory.html
-echo "<html>
-<body>
-<pre>
-" > $DIR/../temp/directory.html
-ls -R $DIR/../ >> $DIR/../temp/directory.html
-echo "</pre></body>
-<html>" >> $DIR/../temp/directory.html
-google-chrome $DIR/../temp/directory.html
+cd "$( dirname "${BASH_SOURCE[0]}" )/../"  
+touch temp/directory.html
+echo "
+<html>
+  <body>
+    <pre>
+" > temp/directory.html
+ls -R >> temp/directory.html
+echo "
+    </pre>
+  </body>
+<html>" >> temp/directory.html
+google-chrome temp/directory.html
 
 
