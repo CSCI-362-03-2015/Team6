62a7a2dfd0251a5e14ea46f2034286f3cd197b92
aaron-monahan <monahanam@g.cofc.edu>
2015-11-09 22:06:58 -0500

    update to sel driver

--- testCasesExecutables/selenium_driver.py
+++ testCasesExecutables/selenium_driver.py
@@ -1,24 +1,42 @@
 import unittest
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+#print sys.argv
+#name = sys.argv[1]
+#print name
+
 class PythonOrgSearch(unittest.TestCase):
+	
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+	
+	def test_search_in_python_org(self):
 
-    def setUp(self):
-        self.driver = webdriver.Firefox()
+		driver = self.driver
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+		driver.implicitly_wait(10)
+
+		driver.get("http://127.0.0.1:8000/eden/default/user/login?_next=%2Feden%2Fdefault%2Findex")
+
+		#driver.get("http://127.0.0.1:8000/eden/default/index")
+		#self.assertIn("Python", driver.title)
+
+ 		elem = driver.find_element_by_name("email")
+		elem.send_keys("test")
+		elem = driver.find_element_by_name("password")
+		elem.send_keys("test" + Keys.RETURN)
+
+		elem = driver.find_element_by_link_text("Login")
+		elem.click
+
+		assert "Invalid login" in driver.page_source
 
 
-    def tearDown(self):
-        self.driver.close()
+	def tearDown(self):
+		self.driver.close()
 
 if __name__ == "__main__":
-    unittest.main()
+
+	unittest.main()
