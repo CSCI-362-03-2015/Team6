a12d407d53baa45c17584bcb64f4101d5d373d03
Sdraugel <draugelsm@g.cofc.edu>
2015-11-19 14:31:39 -0500

    fixed the file so that it works with the runner script
    read my comments

--- testCasesExecutables/pythonDriver.py
+++ testCasesExecutables/pythonDriver.py
@@ -8,10 +8,22 @@ import sys
 import fileinput
 import functools
 from types import FunctionType
-#import eden
+import eden
 
-#~~~~~~~~~Need to figure out how to pass in with shell~~~~~~~~~~~~
-testName = "test1.txt"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#The shell needs to make a txt file calle testName.txt every time a
+#test is run and put the name of the test case file in it. The this driver
+#will parse that file(for 1 line containing the test case) and use it to call
+#the test case.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+testArray = []
+with open("testName.txt", "r")
+    i = 0
+    for line in test:
+        testArray.append(test[i])
+        
+        
+testName = str(testArray[0])
 outcome = ""
 
 #Itterates through the text file and assigns the class, method, inputs, etc. for use in the driver
