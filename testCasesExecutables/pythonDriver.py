9dbdb97e5bb570314027ecbeb65a28c00d98578e
Steven Draugel <draugelsm@g.cofc.edu>
2015-11-23 18:19:26 -0500

    completed python driver and reporting file

--- testCasesExecutables/pythonDriver.py
+++ testCasesExecutables/pythonDriver.py
@@ -11,7 +11,6 @@ from types import FunctionType
 from os.path import *
 import os
 import sys
-#import eden
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #The shell needs to make a txt file calle testName.txt every time a
@@ -19,19 +18,21 @@ import sys
 #will parse that file(for 1 line containing the test case) and use it to call
 #the test case.
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-outcome = ""
 
 #Creates a list of all the test cases
 filelocation = abspath(join(dirname( __file__ ), '..', 'testcases'))
+
+report = abspath(join(dirname( __file__ ), '..', 'reports'))
+reportFile = open(report + "/report.txt", 'a')
+reportFile = open(report + "/report.txt", 'w')
+
 testList = []
 for file in os.listdir(filelocation):
     if file.endswith(".txt"):
         testList.append(file)
 
 #Itterates through the text file and assigns the class, method, inputs, etc. for use in the driver
-
 numTests = len(testList)
-
 for testCase in range(0, numTests):
     i = 0
     testName = abspath(join(dirname( __file__ ), '..', "testcases/" + str(testList[i])))
@@ -42,7 +43,6 @@ for testCase in range(0, numTests):
         j = 0
         for line in ins:
             array.append(line.split(':',1)[-1])
-            print(array[j])
             j+=1
 
         className = array[0].strip().replace(" ", ".")
@@ -50,8 +50,6 @@ for testCase in range(0, numTests):
         inputData = array[2].strip()
         oracle = array[3].strip()
         oracle += ".txt"
-        print("Oracle: " + oracle)
-
         eden = abspath(join(dirname( __file__ ), '..', 'Eden'))
         sys.path.insert(0, eden)
         
@@ -64,10 +62,8 @@ for testCase in range(0, numTests):
 
         #Calls the tested method and places it in result to be compared with the orcale's expected output
         result = x(inputData)
-        print("Result: " + result)
 
         oracleLocation = abspath(join(dirname( __file__ ), '..', 'oracles'))
-        print(oracleLocation)
         array2 = []
         with open(oracleLocation + "\\" + oracle, "r") as filein:
             i = 0
@@ -76,7 +72,6 @@ for testCase in range(0, numTests):
                 i+=1
 
         expectedOutput = array2[2]
-        print("Expected: " + expectedOutput)
 
         if (result == expectedOutput):
             outcome = "PASS"
@@ -84,5 +79,6 @@ for testCase in range(0, numTests):
         else:
             outcome = "FAIL"
 
-        #return outcome
-        print(outcome)
+        reportFile.write("Class Name: " + className + ", Method Name: " +
+                         methodName + ", Input: " + inputData + ", Oracle: " + oracle + ", Result: " + outcome)
+        reportFile.close()
