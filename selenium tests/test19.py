468dc964eb6a624ed9c49f8f1105c80caca0c061
Aaron <monahanam@g.cofc.edu>
2015-11-30 21:20:19 -0500

    sel test19

--- selenium tests/test19.py
+++ selenium tests/test19.py
@@ -16,7 +16,7 @@ class PythonOrgSearch(unittest.TestCase):
 
 		driver.get("http://127.0.0.1:8000/eden/default/about")
 
-		assert "nursix-1.1.0-devel-2831-gf6b46f0 (2015-09-29 09:09:29)" in driver.page_source
+		assert "Public URL" in driver.page_source
 
 
 	def tearDown(self):
