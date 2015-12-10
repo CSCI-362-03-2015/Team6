a4d1ee9549349a827bbf4a8ac6172c15191a89bf
Steven Draugel <draugelsm@g.cofc.edu>
2015-12-10 10:19:52 -0500

    testCases

---
+++ testAutomation/testCases/selTests/test18.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+	
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+	
+	def test_search_in_python_org(self):
+
+		driver = self.driver
+
+		driver.implicitly_wait(10)
+
+		driver.get("http://127.0.0.1:8000/eden/inv/recv")
+
+		assert "Received/Incoming Shipments" in driver.page_source
+
+
+	def tearDown(self):
+		self.driver.close()
+
+if __name__ == "__main__":
+
+	unittest.main()
