5c0bbda17a7e2ac30c9faa5e2d074905f18512dd
Steven Draugel <draugelsm@g.cofc.edu>
2015-12-10 11:20:47 -0500

    fixed runner

--- testAutomation/scripts/runAllTests.sh
+++ testAutomation/scripts/runAllTests.sh
@@ -4,11 +4,11 @@ echo Running all testcases.......
 sleep 3
 
 #Runs all of the Python tests
-python testCasesExecutables/pythonDriver.py
+python ../testCasesExecutables/pythonDriver.py
 sleep 2
 
 echo Opening default browser with results....
 sleep 3
 
 #Opens a browser and displays the results of pythonDriver.py
-python testCasesExecutables/webbuilder.py
+python ../testCasesExecutables/webbuilder.py
