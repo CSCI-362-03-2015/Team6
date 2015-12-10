e1192154ff9ba019fee67eb80533392374f2bfa4
Steven Draugel <draugelsm@g.cofc.edu>
2015-12-10 11:19:57 -0500

    moved test runner

--- testAutomation/scripts/runAllTests.sh
+++ testAutomation/scripts/runAllTests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo Running all testcases.......
-sleep 1
+sleep 3
 
 #Runs all of the Python tests
-python ../testCasesExecutables/pythonDriver.py
-sleep 1
+python testCasesExecutables/pythonDriver.py
+sleep 2
 
 echo Opening default browser with results....
-sleep 1
+sleep 3
+
 #Opens a browser and displays the results of pythonDriver.py
-python ../testCasesExecutables/webbuilder.py
+python testCasesExecutables/webbuilder.py
