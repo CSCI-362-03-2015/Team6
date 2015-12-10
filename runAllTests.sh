fcbbff56073539146ad04d2e9e403e1c4d325114
Steven Draugel <draugelsm@g.cofc.edu>
2015-12-10 07:53:39 -0500

    fixed the framework structure

--- runAllTests.sh
+++ runAllTests.sh
@@ -4,10 +4,10 @@ echo Running all testcases.......
 sleep 1
 
 #Runs all of the Python tests
-python ../testCasesExecutables/pythonDriver.py
+python ./testCasesExecutables/pythonDriver.py
 sleep 1
 
 echo Opening default browser with results....
 sleep 1
 #Opens a browser and displays the results of pythonDriver.py
-python ../testCasesExecutables/webbuilder.py
+python ./testCasesExecutables/webbuilder.py
